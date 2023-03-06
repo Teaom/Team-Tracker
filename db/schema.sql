e560098cb87416e2b9571e5841f85b2a3e0945c2
Teaom <dclarkjenkins87@gmail.com>
2023-03-06 14:50:53 -0700

    Added schema.sql for tables

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS inventory_db;
+CREATE DATABASE inventory_db;
+USE inventory_db;
+
+CREATE TABLE role (
+    id INTEGER NOT NULL AUTOINCREMENT,
+    title VARCHAR(50) NOT NULL,
+    salary DECIMAL (10, 0) NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    department_name VARCHAR(50) NOT NULL,
+    PRIMARY KEY
+
+);
