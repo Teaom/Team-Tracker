b9c4d41409464e0e3eb756f46e81213eeb24d819
Teaom <dclarkjenkins87@gmail.com>
2023-03-08 16:00:29 -0700

    Added seed.sql with roles and people

---
+++ db/seed.sql
@@ -0,0 +1,30 @@
+INSERT INTO ROLE(title, salary, department_id)
+VALUES ('Head Coach', 200000, NULL);
+INSERT INTO ROLE(title, salary, department_id)
+VALUES ('Offensive Coordinator', 150000, 1);
+INSERT INTO ROLE(title, salary, department_id)
+VALUES ('Defensive Coordinator', 150000, 2);
+INSERT INTO ROLE(title, salary, department_id)
+VALUES ('Special Teams Coordinator', 100000, 3);
+INSERT INTO ROLE(title, salary, department_id)
+VALUES ('QB Coach', 100000, 1);
+INSERT INTO ROLE(title, salary, department_id)
+VALUES ('CB Coach', 100000, 2);
+INSERT INTO ROLE(title, salary, department_id)
+VALUES ('Punters Coach', 75000, 3);
+
+INSERT INTO  employee (first_name, last_name, role_id, manager_id)
+VALUES ('Daniel', 'Jenknins', )
+INSERT INTO  employee (first_name, last_name, role_id, manager_id)
+INSERT INTO  employee (first_name, last_name, role_id, manager_id)
+INSERT INTO  employee (first_name, last_name, role_id, manager_id)
+INSERT INTO  employee (first_name, last_name, role_id, manager_id)
+INSERT INTO  employee (first_name, last_name, role_id, manager_id)
+INSERT INTO  employee (first_name, last_name, role_id, manager_id)
+
+INSERT INTO department(department_name)
+VALUES ('Offense');
+INSERT INTO department(department_name)
+VALUES ('Defense');
+INSERT INTO department(department_name)
+VALUES ('Special Teams');
