ca6aab081f027d824470c251dc10ace29c391ae6
Teaom <dclarkjenkins87@gmail.com>
2023-03-08 22:04:43 -0700

    installed console.table

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const consoleTable = require('console.table');
 const util = require('util');
 
 let connection = mysql.createConnection({
     host: 'localhost',
-    port: 3306,
+    port: 3001,
     user: 'root',
-    password: 'password',
+    password: 'tnomerf',
     database: 'employee_DB'
 });
 
