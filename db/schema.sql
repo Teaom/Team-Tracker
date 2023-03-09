dbc4f5f4519edb3f9cfd60076092ca2d0ec6b4a7
Teaom <dclarkjenkins87@gmail.com>
2023-03-09 00:12:18 -0700

    title

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,6 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    title VARCHAR(50) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
     PRIMARY KEY (id)
