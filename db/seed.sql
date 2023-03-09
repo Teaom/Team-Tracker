848513c106be8034019f66948afb8295949b1780
Teaom <dclarkjenkins87@gmail.com>
2023-03-08 21:49:16 -0700

    reinstalled inquirer

--- db/seed.sql
+++ db/seed.sql
@@ -1,14 +1,14 @@
-INSERT INTO ROLE(title, salary, department_id)
+INSERT INTO role(title, salary, department_id)
 VALUES ('Head Coach', 200000, NULL);
-INSERT INTO ROLE(title, salary, department_id)
+INSERT INTO role(title, salary, department_id)
 VALUES ('Offensive Coordinator', 150000, 1);
-INSERT INTO ROLE(title, salary, department_id)
+INSERT INTO role(title, salary, department_id)
 VALUES ('Defensive Coordinator', 150000, 2);
-INSERT INTO ROLE(title, salary, department_id)
+INSERT INTO role(title, salary, department_id)
 VALUES ('Special Teams Coordinator', 100000, 3);
-INSERT INTO ROLE(title, salary, department_id)
+INSERT INTO role(title, salary, department_id)
 VALUES ('QB Coach', 100000, 1);
-INSERT INTO ROLE(title, salary, department_id)
+INSERT INTO role(title, salary, department_id)
 VALUES ('CB Coach', 100000, 2);
 
 INSERT INTO  employee (first_name, last_name, role_id, manager_id)
