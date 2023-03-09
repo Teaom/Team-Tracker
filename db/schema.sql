c2160c06cc1b3823cd5b9f789a17b6cf45645b1e
Teaom <dclarkjenkins87@gmail.com>
2023-03-08 23:21:54 -0700

    reinstalled cosole.table

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE DATABASE employee_DB;
 USE employee_DB;
 
 CREATE TABLE role (
-    id INTEGER NOT NULL AUTOINCREMENT,
+    id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(50) NOT NULL,
     salary DECIMAL (10, 0) NOT NULL,
     department_id INT,
@@ -23,5 +23,4 @@ CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(50) NOT NULL,
     PRIMARY KEY
-
 );
